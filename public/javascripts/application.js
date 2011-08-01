ab977f5ad779c8783f26bdf4687e5bf308fb12a0
carinabs8 <carinabs8@carinabs8.(none)>
2011-08-01 11:38:40 -0400

    desvincula js da view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  $("#upload").click(function(){
+    $("#map").dialog({ modal: true, width: 720, position: 'top' });
+  })
+})
 function get_and_save_coordenates(vaga, vaga_id) {
   var vaga = vaga
   var vaga_id = vaga_id
   //var position = jQuery("#"+vaga).position();
   jQuery("#"+vaga).draggable();
-  jQuery("#"+vaga).bind('mouseenter mouseleave',function(e){ 
+  jQuery("#"+vaga).bind('mouseenter mouseleave',function(e){
     var positionX = this.offsetTop;
     var positionY = this.offsetLeft;
     $("#salva-"+vaga).load('/vagas/update_coordendas/'+vaga_id + "/" + positionX +"/"+ positionY);
