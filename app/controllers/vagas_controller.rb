70f36c58e68c7be38737a85067834187d027de70
carinabs8 <carinabs8@carinabs8.(none)>
2011-08-02 15:03:50 -0400

    retira debugger

--- app/controllers/vagas_controller.rb
+++ app/controllers/vagas_controller.rb
@@ -1,22 +1,22 @@
 class VagasController < ApplicationController
-  
+
   def index
     @search = Vaga.search(params[:search])
     @vagas = @search.paginate(:all, :page => params[:page], :order =>"updated_at DESC")
   end
-  
+
   def show
     @vaga = Vaga.where(:codigo => params[:id]).first
   end
-  
+
   def new
     @vaga = Vaga.new
   end
-  
+
   def edit
     @vaga = Vaga.where(:codigo => params[:id]).first
   end
-  
+
   def update
     @vaga = Vaga.find(params[:id])
     if @vaga.update_attributes(params[:vaga])
@@ -26,20 +26,19 @@ class VagasController < ApplicationController
       render :edit
     end
   end
-  
+
   def coordenates
     @vaga = Vaga.find(params[:id])
   end
-  
+
   def update_coordendas
     @vaga = Vaga.find(params[:vaga_id])
     @vaga.eixo_x = params[:eixo_x]
     @vaga.eixo_y = params[:eixo_y]
     @vaga.save!
-    puts @bu.inspect
     render :partial => "update_coordendas", :layout => false
   end
-  
+
   def create
     map = Map.where(:codigo => params[:vaga][:map_id]).first
     @vaga = Vaga.new(params[:vaga])
@@ -51,7 +50,7 @@ class VagasController < ApplicationController
       render :new
     end
   end
-  
+
   def destroy
     @vaga = Vaga.find(params[:id])
     @vaga.destroy
