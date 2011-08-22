c4e154ef33c1586c3dda3928a52dbafefa3f975f
carinabs8 <carinabs8@gmail.com>
2011-08-21 22:33:45 -0400

    refactory

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -24,17 +24,18 @@ $(function(){
   $('#abas').tabs();
 });
 
+//Automatic Reload =P
 $(function(){
-  if ($("#vaga_of_map").length > 0){
-    setTimeout(updateVagas, 10000);
+  if ($("#vacancy_of_map").length > 0){
+    setTimeout(updateVacancies, 10000);
   }
 });
 
-function updateVagas(){
-  var map_id = $("#vaga_of_map").attr("map_id");
-  var vaga_false = $("#vaga_of_map p a[status=false]").length
-  var vaga_true = $("#vaga_of_map p a[status=true]").length
+function updateVacancies(){
+  var map_id = $("#vacancy_of_map").attr("map_id");
+  var vacancy_unavailable = $("#vacancy_of_map p a[status=false]").length
+  var vacancy_available = $("#vacancy_of_map p a[status=true]").length
   
-  $.getScript("/reload_map.js?map_id=" + map_id + "&qt_false=" + vaga_false + "&qt_true=" + vaga_true)
-  setTimeout(updateVagas, 10000);
+  $.getScript("/reload_map.js?map_id=" + map_id + "&qt_false=" + vacancy_unavailable + "&qt_true=" + vacancy_available)
+  setTimeout(updateVacancies, 10000);
 }
