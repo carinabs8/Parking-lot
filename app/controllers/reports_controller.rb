d57a2ef0628f59f617219137d8edbb5b03ee2eea
carinabs8 <carinabs8@gmail.com>
2011-09-05 22:01:23 -0400

    report when default eql the current date

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,11 @@
 class ReportsController < ApplicationController
   before_filter :require_user
+  before_filter :get_all
   
   def index
     @report_true      = Report.analitic.status(true)
     @report_false     = Report.analitic.status(false)
-    
-    @vagas_name_true  = @vagas_name_false = []
-    @qt_true          = []
-    @qt_false         = []
-    
+ 
     hash_report_true  = @report_true.count(:vaga_id, :group => :vaga_id)
     hash_report_false = @report_false.count(:vaga_id, :group => :vaga_id)
     
@@ -18,7 +15,15 @@ class ReportsController < ApplicationController
     hash_report_true.select{|k,v| @qt_true << v}
     hash_report_false.select{|k,v| @qt_false  << v}
     
-    @total = @qt_true.concat(@qt_false)
+    @total = @qt_true + @qt_false
     @total = @total.inject{|sum, n| sum+n}
   end
+  
+  private
+    def get_all
+      @vagas_name_true  = []
+      @vagas_name_false = []
+      @qt_true          = []
+      @qt_false         = []
+    end
 end
