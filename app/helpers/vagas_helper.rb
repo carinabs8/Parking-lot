b4cf1b036fce231f815d09ccd19ab988f02a2bf2
carinabs8 <carinabs8@gmail.com>
2011-09-11 00:48:15 -0400

    show vaga's status

--- app/helpers/vagas_helper.rb
+++ app/helpers/vagas_helper.rb
@@ -1,2 +1,9 @@
 module VagasHelper
+  def check_box_status(cod_arduino, vaga_status)
+    if cod_arduino == StatusControll::RESTRICTED
+     raw  "#{label(:vaga, :status)} #{check_box :vaga, :status, :checked => true}"
+    elsif cod_arduino == StatusControll::RESTRICTED || vaga_status.nil?
+     raw  "#{label(:vaga, :status)} #{check_box :vaga, :status, :checked => false}"
+    end
+  end
 end
